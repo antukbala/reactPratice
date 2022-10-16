58fc48630fe031d81b283e9b32fd1a8641d3d188
antukbalaTL <amit.kumar@trucklagbe.com>
2022-10-16 18:20:12 +0600

    till 42

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      apiList: [
+        {
+          API: "TL Ops",
+          Link: 'n.trucklagbe.com'
+        },
+        {
+          API: "Notify",
+          Link: 'n.trucklagbe.com'
+        },
+        {
+          API: "App API",
+          Link: 'n.trucklagbe.com'
+        },
+        {
+          API: "Admin Get",
+          Link: 'n.trucklagbe.com'
+        },
+        {
+          API: "Admin Post",
+          Link: 'n.trucklagbe.com'
+        },
+        {
+          API: "GPS",
+          Link: 'n.trucklagbe.com'
+        },
+        {
+          API: "BKASH pgw",
+          Link: 'n.trucklagbe.com'
+        },
+        
+      ]
+    }
+  }
+
+  // async componentDidMount() {
+  //   try {
+  //     const response = await (await fetch('https://api.publicapis.org/entries')).json();
+  //     this.setState({apiList: response.entries});
+  //   } catch (error) {
+  //     console.log(error);
+  //     return;
+  //   }
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <input className='search-box' type='search' placeholder='search name' onChange={(event)=>{
+            const searchString = event.target.value.toLocaleLowerCase();
+            const searchResult = this.state.apiList.filter((item)=>{
+              return item.API.toLocaleLowerCase().includes(searchString);
+            });
+            this.setState(()=>{
+              return { apiList : searchResult };
+            })
+          }}/>
+          <img src={logo} className="App-logo" alt="logo" />
+          {this.state.apiList.map((api)=>{
+            return <div key={api.API}><h1>{api.API}, {api.Link}</h1></div>;
+          })}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
